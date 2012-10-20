432607b012d1179906afcc51a2782cbe8374ea98
Soumya Mandi <soumyamandi@gmail.com>
2012-10-20 16:38:46 +0530

    debugged , logic error with passing addresses as arguments

--- list.c
+++ list.c
@@ -18,28 +18,29 @@ void print_nodes( node* head ){
 }
 
 
-void initialize_list( node* head,node*current,int arr[],int size ) {
+int* initialize_list( node* head,int arr[],int size ) {
 		int counter = 0 ;
+		node* oldhead = head ;
+		node* current = head ;
 		printf("hello\n");
 		while( counter < size ) {
 			
 			current = ( node* )malloc( sizeof( node ) ) ;
-			printf("*(arr+counter) is : %d\n",*(arr+counter) ) ;
+			//printf("*(arr+counter) is : %d\n",*(arr+counter) ) ;
 			current->value = *(arr+counter) ;
 			current->next = head ;
 			head = current ;
-			printf("cur->val is : %d\n", current->value ) ;
+			//printf("cur->val is : %d\n", current->value ) ;
 			counter++ ;
 			
 		}
-		printf("head->value after init is : %d\n", head->value ) ;
+		//printf("head->value after init is : %d\n", head->value ) ;
+		return head ;
 }
 int main( int argc,char** argv ) {
 	int arr[] = { 1,2,3,4,5 } ;
-	node* head,*curr ;// = (node*)0;//= ( node* )malloc( sizeof( node ) ) ;
-	initialize_list( head,curr,arr,5 ) ;
-	printf("head->value is : %d\n", head->value ) ;
-	printf("initialized\n") ;
+	node* head;// = (node*)0;//= ( node* )malloc( sizeof( node ) ) ;
+	head = initialize_list( head,arr,5 ) ;
 	print_nodes( head ) ;
 
 	return 0 ;
