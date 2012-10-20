de37c7f6d210a4d67d93f2e5b459a17e7b4476fc
Soumya Mandi <soumyamandi@gmail.com>
2012-10-20 17:34:32 +0530

    do the reverse of the linked list

---
+++ reverse.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+typedef struct {
+	struct node* next ;
+	int value ;
+
+} node ;
+/* given a node prints all the nodes */
+/* parameters
+	@head :
+		head of the linked list
+*/
+void print_nodes( node* head ){
+	node* oldhead = head ;
+	while( head ) {
+		printf("%d-> ",head->value ) ;
+		head = head->next ;
+
+	}
+	printf("\n") ;
+}
+
+
+node* reverse( node* head ) {
+	node* current ;
+	current = head ;
+
+}
+
+
+/* given an array and a head initializes the link list and returns the head */
+/* parameters
+	@head :
+	    the head of the list which is to be returned.
+	@arr :
+		the array whose values are to be put into the list
+	@size :
+		the size of the array
+*/
+node* initialize_list( node* head,int arr[],int size ) {
+		int counter = 0 ;
+		node* current = head ;
+		while( counter < size ) {
+			
+			current = ( node* )malloc( sizeof( node ) ) ;
+			//printf("*(arr+counter) is : %d\n",*(arr+counter) ) ;
+			current->value = *(arr+counter) ;
+			current->next = head ;
+			head = current ;
+			//printf("cur->val is : %d\n", current->value ) ;
+			counter++ ;
+			
+		}
+		//printf("head->value after init is : %d\n", head->value ) ;
+		return head ;
+}
+int main( int argc,char** argv ) {
+	int arr[] = { 1,2,3,4,5 } ;
+	node* head;// = (node*)0;//= ( node* )malloc( sizeof( node ) ) ;
+	head = initialize_list( head,arr,5 ) ;
+	printf("The nodes in the list is : \n") ;
+	print_nodes( head ) ;
+
+	printf("Going to reverse the linked list * * * * * \n") ;
+	head = reverse( head ) ;
+
+	return 0 ;
+} 
